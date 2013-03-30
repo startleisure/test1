e6a03654355bf3d207d93c711be5a987cec0df40
ken <startleisure@gmail.com>
2013-03-30 07:03:04 -0700

    modified fetch_gf.py success version

--- python_test/fetch_gf.py
+++ python_test/fetch_gf.py
@@ -8,7 +8,13 @@ import httplib
 import urllib2
 import re
 
+stock_ids = ( 1111, 2485, 2330, 0)
+
 print 'Hello World'
+def displaymatch(match):
+    if match is None:
+        return None
+    return '<Match: %r, groups=%r>' % (match.group(), match.groups())
 
 def debug_print( s, msg = None):
 	print "DEBUG", msg, s
@@ -27,40 +33,60 @@ def get_site( stkID = 2330, month = 1, day = 1, year = 1985, start = 0, num = 30
 
 
 httplib.HTTPConnection.debuglevel = 1
-stock_ids = ( 2485, 2330)
 #stock_ids = ( 2485, 2324, 8078, 2311, 2330, 2891, 8926)
 
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 
-# For finding stock price
-iRE_price = re.compile( r".*nowrap><b>([\d.]+)<.*", \
-						re.I | re.U | re.M | re.S )
+# date, open, high, low, close, volume
+#<td class="lm">Jun 8, 2012
+#<td class="rgt">77.00
+#<td class="rgt">78.70
+#<td class="rgt">77.00
+#<td class="rgt">77.90
+#<td class="rgt rm">34,628,000
 
-# pattern: >2330</a><br><a href="/pf/pfsel?stocklist= 
-# For finding stock name
-# synonym e = r'.*>\d+(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
-e = ".*>\d+" + r'(.+)</a><br><a href="/pf/pfsel\?stocklist=.*'
-debug_print( e, "for name" )
-iRE_name = re.compile( e, re.I | re.U | re.M | re.S )
+pattern = r'<td class="lm">(.+)' +  \
+           '\s<td class="rgt">([\d\.]+)' + \
+           '\s<td class="rgt">([\d\.]+)' + \
+           '\s<td class="rgt">([\d\.]+)' + \
+           '\s<td class="rgt">([\d\.]+)' + \
+           '\s<td class="rgt rm">([\d\,]+)'
+
+reg_price = re.compile( pattern)
 
 
 
 for stock_id in stock_ids:
     # Get web page content
-    content = opener.open( 'http://tw.stock.yahoo.com/q/q?s=' + \
-                           str( stock_id ) ).read()
+    urlsite = get_site(stock_id, 1, 31, 1986, 0, 30)
+    debug_print(urlsite)
+    content = opener.open( urlsite ).read()
+    f = open( str(stock_id)+'.log', 'w')
+    stock_data = reg_price.findall( content )
+    length = len(stock_data)
+    if (length is 0 ):
+        print "stock_id: "+ str(stock_id) + " not found! "
+        continue
+    
+    print length 
+    for i in range(length):
+        print str(stock_data[i])
+        f.write(str(stock_data[i]))
+        f.write("\n")
 
+    print "here"
+
+    #f.write(stock_data)
     # Print the whole content for debugging
     #print content
-
-    stock_price = iRE_price.match( content ).groups()[ 0 ]
-    stock_name =  unicode( iRE_name.match( content ).groups()[ 0 ], "BIG5" )
-
+    
+    #stock_name =  unicode( iRE_name.match( content ).groups()[ 0 ], "BIG5" )
+    
     # Print result
-    print "%d\t%.2f" % ( int( stock_id ), \
-                             float( stock_price ) )
+    #print "%d\t%.2f" % ( int( stock_id ), \
+    #                         float( stock_price ) )
     #print "%d\t%s\t%.2f" % ( int( stock_id ), stock_name, \
     #                         float( stock_price ) )
 
