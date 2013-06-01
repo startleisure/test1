aa3711f102dd54c38636345e30c5ef2bcdeec308
ken <startleisure@gmail.com>
2013-05-31 19:52:18 -0700

    new file:     statistics_predictor

---
+++ statistic_predictor/main.cc
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include "astock.h"
+#include <climits>
+
+
+int main(int argc, char *argv[])
+{
+
+    cout << "Stock Statistic Predictor v1.0: " << endl;
+    cout << "sz: " << sizeof(astock_t) << endl;
+
+    return 0;
+}
